abaf94fa4a7580b00cc370e446d3d6cfdf1bcacf
rigood <rigood@naver.com>
2022-12-11 17:57:35 +0900

    Fix: 슬라이더 드래그 시 이전, 다음 버튼 노출/숨김 버그 수정

--- src/components/Common/Slider.jsx
+++ src/components/Common/Slider.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import { useRef } from "react";
 import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -8,8 +8,8 @@ import {
 
 function Slider({ children }) {
   const scrollRef = useRef(null);
-  const leftButtonRef = useRef(null);
-  const rightButtonRef = useRef(null);
+  const PrevButtonRef = useRef(null);
+  const NextButtonRef = useRef(null);
 
   const isDragStart = useRef(false);
   const isDragging = useRef(false);
@@ -41,49 +41,61 @@ function Slider({ children }) {
     isDragging.current = false;
 
     autoSlide();
-    handleButtonShowHide(scrollRef.current.scrollLeft);
   };
 
   const handleButtonShowHide = (scrollLeftValue) => {
     const { scrollWidth, clientWidth } = scrollRef.current;
     const maxScrollableWidth = scrollWidth - clientWidth;
 
-    leftButtonRef.current.style.display = scrollLeftValue > 0 ? "flex" : "none";
-    rightButtonRef.current.style.display =
+    PrevButtonRef.current.style.display = scrollLeftValue > 0 ? "flex" : "none";
+    NextButtonRef.current.style.display =
       maxScrollableWidth - scrollLeftValue > 1 ? "flex" : "none";
   };
 
   const autoSlide = () => {
     const positionDiffAbs = Math.abs(positionDiff.current);
-    const slideWidth = scrollRef.current.clientWidth;
+
+    const slider = scrollRef.current;
+    const slideWidth = slider.clientWidth;
     const valDiff = slideWidth - positionDiffAbs;
+
+    // 이전 슬라이드 없는 경우 종료
+    if (slider.scrollLeft <= 0) return;
+
+    // 다음 슬라이드 없는 경우 종료
+    const maxScrollableWidth = slider.scrollWidth - slider.clientWidth;
+    if (maxScrollableWidth - slider.scrollLeft < 1) return;
+
+    let scrollLeftWidth;
 
     // 오른쪽으로 스크롤한 경우
     if (positionDiff.current < 0) {
       if (positionDiffAbs > slideWidth / 4) {
-        scrollRef.current.scrollLeft += valDiff;
+        scrollLeftWidth = scrollRef.current.scrollLeft += valDiff;
       } else {
-        scrollRef.current.scrollLeft -= positionDiffAbs;
+        scrollLeftWidth = scrollRef.current.scrollLeft -= positionDiffAbs;
       }
     }
 
     // 왼쪽으로 스크롤한 경우
     if (positionDiff.current > 0) {
       if (positionDiffAbs > slideWidth / 4) {
-        scrollRef.current.scrollLeft -= valDiff;
+        scrollLeftWidth = scrollRef.current.scrollLeft -= valDiff;
       } else {
-        scrollRef.current.scrollLeft += positionDiffAbs;
+        scrollLeftWidth = scrollRef.current.scrollLeft += positionDiffAbs;
       }
     }
+
+    handleButtonShowHide(scrollLeftWidth);
   };
 
   const handleButtonClick = (direction) => {
     const slideWidth = scrollRef.current.clientWidth;
     let scrollLeftWidth;
 
-    if (direction === "left") {
+    if (direction === "prev") {
       scrollLeftWidth = scrollRef.current.scrollLeft -= slideWidth;
-    } else if (direction === "right") {
+    } else if (direction === "next") {
       scrollLeftWidth = scrollRef.current.scrollLeft += slideWidth;
     }
 
@@ -92,10 +104,10 @@ function Slider({ children }) {
 
   return (
     <SliderContainer>
-      <LeftButton
+      <PrevButton
         icon={faChevronLeft}
-        ref={leftButtonRef}
-        onClick={() => handleButtonClick("left")}
+        ref={PrevButtonRef}
+        onClick={() => handleButtonClick("prev")}
       />
       <SliderWrapper
         ref={scrollRef}
@@ -106,10 +118,10 @@ function Slider({ children }) {
       >
         {children}
       </SliderWrapper>
-      <RightButton
+      <NextButton
         icon={faChevronRight}
-        ref={rightButtonRef}
-        onClick={() => handleButtonClick("right")}
+        ref={NextButtonRef}
+        onClick={() => handleButtonClick("next")}
       />
     </SliderContainer>
   );
@@ -137,18 +149,18 @@ const SliderButton = styled(FontAwesomeIcon)`
   cursor: pointer;
 `;
 
-const LeftButton = styled(SliderButton)`
+const PrevButton = styled(SliderButton)`
   left: 0;
   display: none;
 `;
 
-const RightButton = styled(SliderButton)`
+const NextButton = styled(SliderButton)`
   right: 0;
 `;
 
 const SliderWrapper = styled.ul`
   display: flex;
-  overflow-x: scroll;
+  overflow-x: hidden;
   scroll-behavior: smooth;
 
   &.dragging {
